16f2901c642fe16f2866ffc012c177b760c2fa48
João <joaog.alves14@gmail.com>
2024-10-03 09:03:33 -0300

    FEAT: add total price to shopping cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,17 +3,22 @@ import React from "react";
 import { useContext } from "react";
 import { useCart } from "../providers/CartProvider";
 import CartListItem from "../components/CartListItem";
+import Button from "../components/button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ padding: 10, gap: 10 }}
       />
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "500" }}>
+        Total: ${total}
+      </Text>
+      <Button text="Checkout" />
     </View>
   );
 };
