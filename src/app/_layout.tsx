44b64f4ca9a1951d76149c7a8c38882a7ad5fd9b
João <joaog.alves14@gmail.com>
2024-10-15 09:00:49 -0300

    FEAT: add basci push notifications

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -13,6 +13,7 @@ import CartProvider from "../providers/CartProvider";
 import AuthProvider, { useAuth } from "../providers/AuthProvider";
 import QueryProvider from "../providers/QueryProvider";
 import { useColorScheme } from "@/src/components/useColorScheme";
+import NotificationProvider from "../providers/NotificationProvider";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -58,14 +59,16 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <AuthProvider>
         <QueryProvider>
-          <CartProvider>
-            <Stack>
-              <Stack.Screen name="(user)" options={{ headerShown: false }} />
-              <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-              <Stack.Screen name="cart" options={{ presentation: "modal" }} />
-            </Stack>
-          </CartProvider>
+          <NotificationProvider>
+            <CartProvider>
+              <Stack>
+                <Stack.Screen name="(user)" options={{ headerShown: false }} />
+                <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+              </Stack>
+            </CartProvider>
+          </NotificationProvider>
         </QueryProvider>
       </AuthProvider>
     </ThemeProvider>
